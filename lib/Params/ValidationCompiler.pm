a088a098c87db756907201d034ba276502e683d3
Dave Rolsky <autarch@urth.org>
2016-11-02 14:30:49 -0500

    Bump version after release

--- lib/Params/ValidationCompiler.pm
+++ lib/Params/ValidationCompiler.pm
@@ -3,7 +3,7 @@ package Params::ValidationCompiler;
 use strict;
 use warnings;
 
-our $VERSION = '0.14';
+our $VERSION = '0.15';
 
 use Params::ValidationCompiler::Compiler;
 
--- lib/Params/ValidationCompiler/Compiler.pm
+++ lib/Params/ValidationCompiler/Compiler.pm
@@ -3,7 +3,7 @@ package Params::ValidationCompiler::Compiler;
 use strict;
 use warnings;
 
-our $VERSION = '0.14';
+our $VERSION = '0.15';
 
 use Carp qw( croak );
 use Eval::Closure qw( eval_closure );
