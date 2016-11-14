f46ba017c5971e9c50b66a9a32c839b443f0f453
Dave Rolsky <autarch@urth.org>
2016-11-13 19:27:40 -0600

    Bump version after release

--- lib/Params/ValidationCompiler/Exceptions.pm
+++ lib/Params/ValidationCompiler/Exceptions.pm
@@ -3,7 +3,7 @@ package Params::ValidationCompiler::Exceptions;
 use strict;
 use warnings;
 
-our $VERSION = '0.18';
+our $VERSION = '0.19';
 
 use Exception::Class (
     'Params::ValidationCompiler::Exception::BadArguments',
