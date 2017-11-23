f38907d90350a82fb63e0bde5480db4d7263b0a2
Dave Rolsky <autarch@urth.org>
2017-11-23 13:03:10 -0600

    Bump version after release

--- lib/Params/ValidationCompiler.pm
+++ lib/Params/ValidationCompiler.pm
@@ -3,7 +3,7 @@ package Params::ValidationCompiler;
 use strict;
 use warnings;
 
-our $VERSION = '0.25';
+our $VERSION = '0.26';
 
 use Params::ValidationCompiler::Compiler;
 
--- lib/Params/ValidationCompiler/Exceptions.pm
+++ lib/Params/ValidationCompiler/Exceptions.pm
@@ -3,7 +3,7 @@ package Params::ValidationCompiler::Exceptions;
 use strict;
 use warnings;
 
-our $VERSION = '0.25';
+our $VERSION = '0.26';
 
 use Exception::Class (
     'Params::ValidationCompiler::Exception::BadArguments',
